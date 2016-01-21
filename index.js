7dd4d23d24a98cd7ae5b00910dacc7b9570e28fb
yaniv kessler <yanivk@gmail.com>
2016-01-21 13:31:59 -0500

    support search by any tag using -t "k=v", tag names are case insensitive

--- index.js
+++ index.js
@@ -3,12 +3,23 @@
 var AWS = require('aws-sdk');
 var rc = require('rc');
 var config = rc('ec2-instance-lister');
-var searchString;
+var tagName = 'name';
+var tagValue;
 
 if (config.s) {
-	searchString = config.s;
+	tagValue = config.s;
+} else if (config.tag || config.t) {
+	config.tag = config.tag || config.t
+	var split = config.tag.split('=')
+	
+	if (split.length !== 2) {
+		throw new Error('invalid tag expression, must be key=value. Input was ' + config.tag)
+	}
+
+	tagName = split[0].toLowerCase()
+	tagValue = split[1]
 } else {
-	searchString = config.searchString;
+	tagValue = config.searchString;
 }
 
 var ec2params = {
@@ -48,6 +59,8 @@ function onResponse(err, data) {
 	}
 
 	var result = [];
+	
+	console.error('NOTE: only ips are written to stdout, the rest is written to stderr.')
 
 	for (var i = 0; i < res.length; i++) {
 		var group = res[i].Instances;
@@ -56,9 +69,10 @@ function onResponse(err, data) {
 				var instance = group[j];
 				var tags = instance.Tags;
 				for (var k = 0; k < tags.length; k++) {
-					if ((tags[k].Key === 'Name') && (tags[k].Value.indexOf(searchString) > -1)) {
+					if ((tags[k].Key.toLowerCase() === tagName) && (tags[k].Value.indexOf(tagValue) > -1)) {
 						var ip = instance[ipField]
 						if (ip) {
+							console.error('instance id [%s]', instance.InstanceId)
 							console.log(ip);
 						}
 					}
