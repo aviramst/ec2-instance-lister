a93e3ee61e193e29eb4d76e1bf52794c28c4317b
Aviram Sterman <avirams@ironsrc.com>
2016-01-28 00:20:41 +0200

    Fix some mess and upgrade dependencies

--- index.js
+++ index.js
@@ -3,48 +3,54 @@
 var AWS = require('aws-sdk');
 var rc = require('rc');
 var config = rc('ec2-instance-lister');
+
 var tagName = 'name';
 var tagValue;
+var ipField;
 
-if (config.s) {
-	tagValue = config.s;
-} else if (config.tag || config.t) {
-	config.tag = config.tag || config.t;
-	var split = config.tag.split('=');
-	
-	if (split.length !== 2) {
-		throw new Error('invalid tag expression, must be key=value. Input was ' + config.tag);
-	}
+start();
 
-	tagName = split[0].toLowerCase();
-	tagValue = split[1];
-} else {
-	tagValue = config.searchString;
-}
+function start() {
+	if (config.s) {
+		tagValue = config.s;
+	} else if (config.tag || config.t) {
+		config.tag = config.tag || config.t;
+		var split = config.tag.split('=');
 
-var ec2params = {
-	Filters: [{
-		Name: 'instance-state-code',
-		Values: [
-			'16'
-		]
-	}]
-};
+		if (split.length !== 2) {
+			throw new Error('invalid tag expression, must be key=value. Input was ' + config.tag);
+		}
 
-var credentials = config.aws
-
-if (config.account) {
-	if (config[config.account]) {
-		credentials = config[config.account];
+		tagName = split[0].toLowerCase();
+		tagValue = split[1];
 	} else {
-		return console.error('you specified an account %s, but the config did not include actual credential data, try adding config.[account name] = { ... credentials here ...}', config.account);
+		tagValue = config.searchString;
 	}
+
+	var ec2params = {
+		Filters: [{
+			Name: 'instance-state-code',
+			Values: [
+				'16'
+			]
+		}]
+	};
+
+	var credentials = config.aws;
+
+	if (config.account) {
+		if (config[config.account]) {
+			credentials = config[config.account];
+		} else {
+			return console.error('you specified an account %s, but the config did not include actual credential data, try adding config.[account name] = { ... credentials here ...}', config.account);
+		}
+	}
+
+	ipField = config.ipType === 'private' ? 'PrivateIpAddress' : 'PublicIpAddress';
+
+	var ec2 = new AWS.EC2(credentials);
+	ec2.describeInstances(ec2params, onResponse);
 }
-
-var ipField = config.ipType === 'private' ? 'PrivateIpAddress' : 'PublicIpAddress';
-
-var ec2 = new AWS.EC2(credentials);
-ec2.describeInstances(ec2params, onResponse);
 
 function onResponse(err, data) {
 	if (err) {
@@ -58,23 +64,22 @@ function onResponse(err, data) {
 		return;
 	}
 
-	var result = [];
-	
 	console.error('NOTE: only ips are written to stdout, the rest is written to stderr.');
 
 	for (var i = 0; i < res.length; i++) {
 		var group = res[i].Instances;
-		if (group !== undefined) {
-			for (var j = 0; j < group.length; j++) {
-				var instance = group[j];
-				var tags = instance.Tags;
-				for (var k = 0; k < tags.length; k++) {
-					if ((tags[k].Key.toLowerCase() === tagName) && (tags[k].Value.indexOf(tagValue) > -1)) {
-						var ip = instance[ipField];
-						if (ip) {
-							console.error('instance id [%s]', instance.InstanceId);
-							console.log(ip);
-						}
+		if (!group) {
+			continue;
+		}
+		for (var j = 0; j < group.length; j++) {
+			var instance = group[j];
+			var tags = instance.Tags;
+			for (var k = 0; k < tags.length; k++) {
+				if ((tags[k].Key.toLowerCase() === tagName) && (tags[k].Value.indexOf(tagValue) > -1)) {
+					var ip = instance[ipField];
+					if (ip) {
+						console.error('instance id [%s]', instance.InstanceId);
+						console.log(ip);
 					}
 				}
 			}
